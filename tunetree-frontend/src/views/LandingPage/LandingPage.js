135de0b639582cda45ece23b62df63e216ea15cf
michaelwoodruffdev <michaelwoodruffdev@gmail.com>
2020-05-22 12:18:35 -0500

    implement tree animation on landing page (looks especially good on tablet/mobile)

--- tunetree-frontend/src/views/LandingPage/LandingPage.js
+++ tunetree-frontend/src/views/LandingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styles from './LandingPage.module.css';
+import treeAnimation from '../../globalStyles/treeAnimation.module.css';
 import Button from '../../components/Button/Button.js';
 import { Link } from 'react-router-dom';
 
@@ -17,7 +18,7 @@ class LandingPage extends Component {
                     <h3 className={styles.titleH3}>See where your sounds will go</h3>
                 </div>
                 <div className={styles.infoBox}>
-                    <p className={styles.infoBoxP}>TuneTree lets you collaborate with other musicians, piece by piece, until a tree of creativity is formed. You can see all the directions others have taken your sound, as well as take inspiration from others sounds. Get started today!</p>
+                    <p className={styles.infoBoxP}>TuneTree lets you collaborate with other musicians, piece by piece, until a tree of sound is formed. You can see all the directions others have taken your sound, as well as take inspiration from others sounds. Get started today!</p>
                 </div>
                 <div className={styles.buttonRow}>
                     <Link to="/signin">
@@ -30,6 +31,7 @@ class LandingPage extends Component {
                 <div className={styles.linkRow}>
                     <a href="http://www.github.com/michaelwoodruffdev/tune-tree">Visti Github</a> | <a href="http://www.github.com/michaelwoodruffdev/tune-tree">Continue as Guest</a>
                 </div>
+                <img src="tunetree-tree.png" alt="" className={treeAnimation.tree} />
             </div>
         );
     }
